56babb9e46a0bf4699d31725cbb5f050b5de8360
aarti-sutar <aartisutar1838@gmail.com>
2026-02-24 20:58:04 +0530

    Python string basics practice

--- Second_day.py
+++ Second_day.py
@@ -1,6 +1,60 @@
-name= "Coding Shala"
+name= "Coding"
 print(name)
 city='Thane'
 print(city)
 code='''welcome to Python Series'''
 print(code)
+#String Print
+name = input("Enter your Name")
+print(name)
+#String Concatenation
+first = "Hello"
+second = "Aarti"
+print(first+ " " +second)
+#String Lenght
+name = "Aarti"
+print(len(name))
+#String Indexing
+name = "Aarti"
+print(name[0])
+print(name[1])
+print(name[-1])
+#String Slicing
+st = "Aarti"
+print(st[0:3])
+print(st[3:5])
+print(st[:5])
+print(st[2:])
+print(st[::2]) 
+
+#String Length
+msg = "Aarti"
+print(len(msg))
+
+#String Concatenation (Joining Strings)
+first = "Aarti"
+second = "Deepak"
+third = "Sutar"
+
+result = first+""+second+""+third
+print(result)
+
+word = "Hi!"
+print(word * 3)
+
+msg = "Learning Python is fun"
+print("Python" in msg)
+print("Java" not in msg)
+print("Java" in msg)
+
+name = "Aarti"
+print("Hello"+name)
+
+name = "Aarti"
+age = 25
+print(f"My name is'{name} and i am {age} year old.")
+
+name = "Aarti"
+city = "Thane"
+print("My name is {} and i live in {}.".format(name, city))
+ 
