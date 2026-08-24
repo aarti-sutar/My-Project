e4f31f78df02b0289ee30198cfdac94b65ff5c9a
aarti-sutar <aartisutar1838@gmail.com>
2026-08-24 21:44:11 +0530

    Basic Questions solve

---
+++ Practice_Questions.py
@@ -0,0 +1,19 @@
+# #1) write a Program to input 2 numbers & Print their sum
+
+# first = int(input("enter first : " ))
+# second = int(input("enter second : " ))
+
+# print("sum =", first + second )
+
+# #2) WAP to input side of a square & Print a area
+
+# side = float(input("enter square side : " ))
+
+# print("area =", side ** 2)
+
+#3) WAP to input 2 floating point numbers & print their average.
+
+a = float(input("enter first : "))
+b = float(input("enter second : "))
+
+print("avg =", (a+b)/2)
