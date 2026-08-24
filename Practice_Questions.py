af8d00d9b38da9622fe9871818d01b86042f7804
aarti-sutar <aartisutar1838@gmail.com>
2026-08-24 23:39:18 +0530

    Add Python practice files

--- Practice_Questions.py
+++ Practice_Questions.py
@@ -1,19 +1,21 @@
-# #1) write a Program to input 2 numbers & Print their sum
+#1) write a Program to input 2 numbers & Print their sum
 
-# first = int(input("enter first : " ))
-# second = int(input("enter second : " ))
+first = int(input("enter first : " ))
+second = int(input("enter second : " ))
 
-# print("sum =", first + second )
+print("sum =", first + second )
 
-# #2) WAP to input side of a square & Print a area
+#2) WAP to input side of a square & Print a area
 
-# side = float(input("enter square side : " ))
+side = float(input("enter square side : " ))
 
-# print("area =", side ** 2)
+print("area =", side ** 2)
 
-#3) WAP to input 2 floating point numbers & print their average.
+3) WAP to input 2 floating point numbers & print their average.
 
 a = float(input("enter first : "))
 b = float(input("enter second : "))
 
 print("avg =", (a+b)/2)
+
+
